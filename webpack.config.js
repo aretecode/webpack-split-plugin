ceaab215d6ea419f52f830f55cd5e5c22464af24
Arete Code <aretecode@gmail.com>
2017-05-18 03:33:49 -0700

    👕 prettier the config, 📦📇 metadata

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,60 @@
 var path = require('path')
 var pkg = require('./package.json')
 var webpack = require('webpack')
+var Chunker = require('./Chunker')
 var rootPath = path.resolve(__dirname, '.')
 var alias = {
   src: rootPath + '/src',
 }
+var log = require('fliplog')
 
 var config = {
   entry: {
-    "index": ["./src/index.js"],
+    index: ['./src/index.js'],
   },
   output: {
-    path: rootPath + '/dist',
+    path: rootPath + '/disted',
     publicPath: '/build/',
     filename: '[name].js',
     libraryTarget: 'amd',
     library: 'demo',
   },
   resolve: {
-    alias: alias
+    alias: alias,
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader',
-      }]
-    }, {
-      test: /\.scss|\.css/,
-      use: [{
-        loader: 'style-loader',
-      }, {
-        loader: 'sass-loader'
-      }]
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.scss|\.css/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
   },
   externals: {},
 
   plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify('production')
-      },
+    new Chunker({
+      name: 'eh',
+      minChunks: 0,
+      // pieces: 2,
     }),
-  ]
+  ],
 }
 
 module.exports = config
